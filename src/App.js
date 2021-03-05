455c527306b1c0f0861fd2c9dc26eed89936ab8c
Sunny <official.multifidus@gmail.com>
2021-03-05 11:22:34 +0900

    create customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
-import logo from './logo.svg';
+import React, {Component, PureComponent} from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's develop</h2>
-    </div>
-  );
+const customers = [{
+  'id' : 1,
+  'image' : 'https://placeimg.com/64/64/1',
+  'name' : '홍길동',
+  'birthday' : '19921107',
+  'gender' : '남자',
+  'job' : '직장인'
+},
+{
+  'id' : 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '김유빈',
+  'birthday' : '19921107',
+  'gender' : '남자',
+  'job' : '직장인'
+},
+{
+  'id' : 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '최건희',
+  'birthday' : '19921107',
+  'gender' : '남자',
+  'job' : '직장인'
+}]
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
